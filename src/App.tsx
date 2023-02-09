a331b7e03ad99ef9f09e8f88a9a5f7ccd5d4d6d0
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-02-08 16:25:53 -0800

    a11y h1 + leading

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
       <div className="flex max-w-[325px] flex-col gap-4 rounded-3xl bg-white p-4 shadow-lg">
         <img src={qrcode} alt="qr code" className="rounded-xl" />
         <div className="flex flex-col gap-4 p-3">
-          <p className="text-center text-2xl font-bold text-dark_blue">
+          <h1 className="text-center text-2xl font-bold leading-9 text-dark_blue">
             Improve your front-end skills by building projects
-          </p>
+          </h1>
 
-          <p className="text-center text-grayish_blue/90">
+          <p className="text-center leading-7 text-grayish_blue/90">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
